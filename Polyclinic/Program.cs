76e09181338a6f533052d41f713779f8dda99f6f
operatorIO-stock <vova.chigorin@yandex.com>
2024-11-09 23:30:31 +0300

    new commit

---
+++ Polyclinic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Polyclinic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Укажите количество человек в очереди");
+        int peopleQuery = int.Parse(Console.ReadLine());
+        int waitInMinutes = 10;
+
+        int hourInMinutes = 60;
+
+        int resultHours = (peopleQuery * waitInMinutes) / hourInMinutes;
+        int resultMinutes = (peopleQuery * waitInMinutes) % hourInMinutes;
+
+        Console.WriteLine("Вам предстоит отстоять в очереди: " +
+                        $"{resultHours}h {resultMinutes}m");
+    }
+}
